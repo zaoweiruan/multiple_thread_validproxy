46e8e6eaaceeda43a48572e451a2662cfa951494
flybearonsky <zaoweiruan@sohu.com>
2026-04-10 11:59:09 +0800

    Clean up git tracking: remove unrelated directories (bin/src/include/AGENTS.md) and AI tool configs

--- grok_app/ProfileSerializer.h
+++
@@ -1,7 +0,0 @@
-// ProfileSerializer.h
-#pragma once
-#include "ProfileItem.h"
-#include <boost/json.hpp>
-
-boost::json::object ToJson(const ProfileItem& item);
-ProfileItem FromJson(const boost::json::value& jv);
--- run_test.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-E:\eclipse_workspace\multiple_thread_validproxy\bin\validproxy.exe
-pause
