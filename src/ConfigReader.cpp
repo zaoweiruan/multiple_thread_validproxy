f622f4ff09aa70bb236d9c20d9a4410acdff4285
flybearonsky <zaoweiruan@sohu.com>
2026-08-19 09:53:16 +0800

    feat(config): wire ProxyProcessMonitorConfigParser + serialize proxy_process_monitor

--- src/ConfigReader.cpp
+++ src/ConfigReader.cpp
@@ -25,6 +25,7 @@
 #include "config/sections/AutoTaskConfigParser.h"
 #include "config/sections/NetworkMonitorConfigParser.h"
 #include "config/sections/ProxyConfigParser.h"
+#include "config/sections/ProxyProcessMonitorConfigParser.h"
 
 namespace config {
 
@@ -75,6 +76,7 @@ std::optional<AppConfig> ConfigReader::load(const std::string& configPath) {
     AutoTaskConfigParser().parse(jv, config, exeDir);
     NetworkMonitorConfigParser().parse(jv, config, exeDir);
     ProxyConfigParser().parse(jv, config, exeDir);
+    ProxyProcessMonitorConfigParser().parse(jv, config);
 
     // Step 5: Log SQL queries
     if (!config.sql_query.empty())
